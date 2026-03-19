a4aefa19611c90a0c88f75bd1446eb83e77dfc62
krv4art <github-actions[bot]@users.noreply.github.com>
2026-03-19 10:41:58 +0000

    🚀 Auto-deploy from krv4art/_VC@6aafe07df7527f00c88b7d84739678036d5fbc94
    Source commit: docs: Add Masked Text exercise TZ (8-tier difficulty system)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2630899292"
+    serviceWorkerVersion: "1668419700"
   }
 });
