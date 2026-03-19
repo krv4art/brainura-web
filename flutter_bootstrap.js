acaec346717bc875065365c5fb6405d4fb0b8558
krv4art <github-actions[bot]@users.noreply.github.com>
2026-03-19 11:23:26 +0000

    🚀 Auto-deploy from krv4art/_VC@eafc9ce5ef4d45c34552f3afc678952eca9a40fa
    Source commit: feat: Implement 8-tier difficulty system for 6 exercises

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "817919512"
+    serviceWorkerVersion: "550019609"
   }
 });
